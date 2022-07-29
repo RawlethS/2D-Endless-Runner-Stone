2665e18a8e3c5daae57d6a713dd47ecc3a5962cf
RawlethS <110128957+RawlethS@users.noreply.github.com>
2022-07-29 09:32:30 +0800

    Refined Background Object Loop
    BG Objects basically loop back from left to right with some added randomness with higher scaling on closer objects to the player

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -7,10 +7,6 @@ public class Parallax : MonoBehaviour
     // Depth = 1 is same depth as player
     public float depth = 1;
 
-    Collider m_Collider;
-    Vector3 m_Center;
-    Vector3 m_Size, m_Min, m_Max;
-
     Player player;
 
     private void Awake()
@@ -21,28 +17,8 @@ public class Parallax : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Fetch the Collider from the GameObject
-        m_Collider = GetComponent<Collider>();
-        //Fetch the center of the Collider volume
-        m_Center = m_Collider.bounds.center;
-        //Fetch the size of the Collider volume
-        m_Size = m_Collider.bounds.size;
-        //Fetch the minimum and maximum bounds of the Collider volume
-        m_Min = m_Collider.bounds.min;
-        m_Max = m_Collider.bounds.max;
-        //Output this data into the console
-        OutputData();
-    }
 
-    void OutputData()
-    {
-        //Output to the console the center and size of the Collider volume
-        Debug.Log("Collider Center : " + m_Center);
-        Debug.Log("Collider Size : " + m_Size);
-        Debug.Log("Collider bound Minimum : " + m_Min);
-        Debug.Log("Collider bound Maximum : " + m_Max);
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -54,10 +30,12 @@ public class Parallax : MonoBehaviour
         // going in negative velocity therefore going "backward"
         pos.x -= realVelocity * Time.fixedDeltaTime;
 
-        /*
-        if (pos.x <= 0 - size.x)
-            pos.x = 80;
-        */
+        // As soon as object leaves the camera on the left, it immediately gets put back beyond the right border
+        // + added randomness with a higher scale for closer depth objects to the player
+        if (pos.x <= 0 - transform.localScale.x / 2)
+        {
+            pos.x = Camera.main.transform.localPosition.x * 2 + transform.localScale.x / 2 + Random.Range(0, 1000/depth);
+        }
 
         /*
         if (pos.x <= -25)
